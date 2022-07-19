49f89e44f200fd77bf75068bbb467263d23085e0
srikanta30 <srikantabanerjee30@gmail.com>
2022-07-19 22:03:47 +0530

    Server Changes

--- src/server.js
+++ src/server.js
@@ -19,13 +19,13 @@ app.get("/search", async (req, res) => {
 
             const results = await axios.get(`https://graph.facebook.com/search?type=adinterest&q=${searchQuery}&limit=10000&locale=en_US&access_token=1039939589945829|Xy4mSGgfa0zBPdRL_Lc9ldkmEGc`);
 
-            return res.status(200).json({ success: true, results: [...results.data] });
+            return res.status(200).json({ success: true, data: results.data });
 
         }
 
         else {
 
-            return res.status(200).json({ success: true, results: [] });
+            return res.status(200).json({ success: true, data : [] });
         }
 
     } catch (err) {
